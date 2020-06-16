a2c631fe1498a9c38a74344a0e2e75062b695e46
Fatso31 <artemisbeams@gmail.com>
2020-06-16 19:04:08 +0000

    makes a fish goes away from me

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from pygame.locals import *
 import sys
 
@@ -12,11 +13,26 @@ clock = pygame.time.Clock()
 
 color = (0,127, 255)
 
+fish = pygame.image.load("fish.png")
+fish = pygame.transform.smoothscale(fish,(80,80))
+fishRect =  fish.get_rect()
+fishRect.center = (width//2, height//2)
+
+speed = pygame.math.Vector2(0,10)
+rotation = random.randint(0,360)
+speed.rotate_ip(rotation)
+fish = pygame.transform.rotate(fish, 180-rotation)
+
+def moveFish():
+  fishRect.move_ip(speed)
+
 def main():
   while True:
     clock.tick(60)
     screen.fill(color)
+    screen.blit(fish, fishRect)
     pygame.display.flip()
+    moveFish()
 
 if __name__ == '__main__':
   main()
