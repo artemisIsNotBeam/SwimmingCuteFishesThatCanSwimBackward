6d641b425ca3694804bfe2354c10af9aebbd23ba
Fatso31 <artemisbeams@gmail.com>
2020-06-16 18:19:10 +0000

    made screen appear and added fish image

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-import pygame.examples.aliens as game
+import pygame
+from pygame.locals import *
+import sys
 
-game.main()
+pygame.init()
+screenInfo = pygame.display.Info()
 
-print("hello SwimmingCuteFishesThatCanSwimBackward")
+#set the width and height of the screen
+screenSize = (width, height) = (int(screenInfo.current_w)), (int(screenInfo.current_h))
+screen = pygame.display.set_mode(screenSize)
+clock = pygame.time.Clock()
+
+color = (0,127, 255)
+
+def main():
+  while True:
+    clock.tick(60)
+    screen.fill(color)
+    pygame.display.flip()
+
+if __name__ == '__main__':
+  main()
+
